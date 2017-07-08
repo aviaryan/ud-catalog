d360e25c5b76f44b6cb43f1f7a83cbeb3d1174be
Avi Aryan <avi.aryan123@gmail.com>
2017-07-09 00:21:57 +0530

    init project

---
+++ catalog/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
---
+++ manage.py
@@ -0,0 +1,14 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from catalog import app, db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+# add migrations command interface so we can manipulate db from command line
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    manager.run()
