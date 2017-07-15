b8711d7f728ccfe31f9bb53d539b5bc6e7b91610
Avi Aryan <avi.aryan123@gmail.com>
2017-07-15 14:40:50 +0530

    harcode url in wsgi config

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,11 @@ import sys
 
 sys.path.insert(0, '/var/www/catalog')
 
+
 from catalog import app as application
 
 application.secret_key = 'New secret key. Change it on server'
+
+application.config['SQLALCHEMY_DATABASE_URI'] = (
+    'postgresql://'
+    'catalog:password@localhost/catalog')
